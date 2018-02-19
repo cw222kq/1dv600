b2b0b619288ff95b771740dd586a033cd4f26eff
Charlotte Westerberg <cw222kq@student.lnu.se>
2018-02-19 16:25:58 +0100

    Printing out javascript object I believe'

--- app/dao/LibraryDAO.js
+++ app/dao/LibraryDAO.js
@@ -6,25 +6,57 @@
 
     // Instructions how to use the xml2js
     // https://github.com/Leonidas-from-XIV/node-xml2js
-    var xml2js = require('xml2js').parseString;
+    var xml2js = require('xml2js');
+    var parseString = require('xml2js').parseString;
 
 
     // Use this file to write and read the xml file.
     var LibraryDAO = {
 
         // Get the entire file from the file system.
-        readXMLFile: function(callback) {
-          /*added 18/2 Task2 design fetch books*/
-          fs.readFile("books.xml", function(err,data){
-            if(err){
-              console.log(err);
-            }
-            else{
-              console.log(data.toString());
-            }
-          });
+        readXMLFile: function(callback) { //har slut vinge
+          /*added 18/2 Task2 design fetch books, solved with inspiration from: https://github.com/Leonidas-from-XIV/node-xml2js*/
+              fs.readFile("books.xml", function(err,data){ //har slutvinge
+                if(err){ //har slut vinge
+                  console.log(err);
+                } //slut på if
+                else{ //har slutvinge
+                  /* convert the data to javascript objects and save in result*/
+                  parseString(data, function(err, result){
+                      if(err){ //har slutvinge
+                        console.log(err);
+                      } //slut på if
+                      else if(result){
+                        /*printing out javascript objects*/
+                        console.log("printing out javascript objects*************");
+                        console.log(result.catalog.book);
+                        console.log("The end of printing js objects!!!!!!!!!!!111");
+                        /*converting the javascript objects to JSON and printing it in the console*/
+                        console.log("printing out data in JSON***************")
+                        //console.log(JSON.stringify(result.catalog.book));
+                        console.log("the end of printing data in JSON!!!!!!!!!!!!!!!!");
+                        var promise = new Promise(function(resolve,reject){
+                          if(result){ //har slutvinge
+                            console.log("RESULT");
+                            resolve(JSON.stringify(result.catalog.book));
+                            //console.log(JSON.stringify(result.catalog.book));
+                            return JSON.stringify(result.catolog.book);
+                          } //slut på if
+                          else { //har slutvinge
+                            console.log("REJECT");
+                            reject("none");
+                          } //slut på else
 
-        },
+                        });
+                      }//slut på else if tillagt senare
+
+                    });//slut på else tillagt sernare
+
+                  } //slut på parseString
+
+              }); //slut på fs.readfile
+
+      }, //end of readXMLFile
 
         // Write the entire file from the file system.
         writeXMLFile: function(data) {
--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -9,11 +9,42 @@
     var listOfBooks = [];
 
     /*Adding some books, added 30/1 subtask a*/
-    listOfBooks.push(new Book(1,"IT","Stephen King", "horror",860101, 90,"It is a 1986 horror novel by American author Stephen King..."));
+    /*listOfBooks.push(new Book(1,"IT","Stephen King", "horror",860101, 90,"It is a 1986 horror novel by American author Stephen King..."));
     listOfBooks.push(new Book(2,"Cujo","Stephen King", "horror",810202, 75,"It is a 1981 psychological horror novel about a rabid dog..."));
     listOfBooks.push(new Book(3,"Harry Potter and the Philosopher's Stone","J.K Rowling", "fantasy", 970303, 80," It is the first novel in the Harry Potter series.."));
+    */
+
+
+    /*19/"*/
+    /*var result = new LibraryDAO.readXMLFile();
+    result.forEach(obj =>{
+        listOfBooks.push(catalog.book);
+    listOfBooks.push(new LibraryDAO.readXMLFile();
+  });*/
+  //listOfBooks.push(new LibraryDAO.readXMLFile());
+    /*19/" slut*/
+
 
     module.exports = function (callback, title) { // The title is optional and is only present when searching. (You need yo modify the books.js file first)
+
+          /*19/"*/
+
+          console.log("console loggar read xmlfile");
+          (new LibraryDAO.readXMLFile(),function(err,dataFromFile){
+            console.log("tillbax i getbooksresource");
+            if(err){
+              console.log("ierr");
+              console.log(err);
+            }
+            else{
+              console.log("i elsen i getbooksresource");
+              console.log(dataFromFile);
+              callback(dataFromFile);
+            }
+          });
+          /*19/" slut*/
+
+
           /*added 30/1 subtask a*/
           //console.log(listOfBooks);
 
@@ -23,9 +54,45 @@
           /*added 31/1 subtask c*/
           //callback(JSON.stringify(listOfBooks));
 
+
+
+
+
+
+
+
           /*added 18/2 Task2 design fetch books*/
           //callback(console.log(new LibraryDAO.readXMLFile()));
-          callback(new LibraryDAO.readXMLFile());
+
+          /*console.log(new LibraryDAO.readXMLFile());*/
+          //callback(new LibraryDAO.readXMLFile());
+          //callback(new LibraryDAO.readXMLFile());
+
+          //callback(console.log(new LibraryDAO.readXMLFile()));
+         /*callback(new LibraryDAO.readXMLFile(),function(err,result){
+            if(err){
+
+              console.log(err);
+            }
+            else{
+
+              console.log(result);
+              callback(result);
+            }
+          });*/
+
+          /*
+          new LibraryDAO.readXMLFile(),function(result,err){
+            if(result){
+
+              console.log(result);
+            }
+            else{
+
+              console.log(err);
+            }
+
+          };*/
     };
 
 }());
